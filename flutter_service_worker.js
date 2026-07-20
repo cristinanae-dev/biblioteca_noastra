f33666568c52332bfe9fb06b2825b73c7f63724e
Cristina Nae <cristinanae.dev@gmail.com>
2026-07-20 22:42:55 +0300

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -126,7 +126,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "88dc77c616884ab13b74b0cbbf835914
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1a1a1871b74c315f788511390c465af6",
+"flutter_bootstrap.js": "ba1d47725845358869e3155d07b18279",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
